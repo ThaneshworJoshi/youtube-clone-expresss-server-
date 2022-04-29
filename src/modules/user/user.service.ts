288253f0f34d4a31edefecf30446741e18171108
thaneshwor10@gmail.com <thaneshwor10@gmail.com>
2022-04-29 18:43:41 +0545

    feat: login user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,9 @@
-import { UserModel } from './user.module';
+import { User, UserModel } from './user.module';
 
-export async function createUser(user) {
+export async function createUser(user: Omit<User, 'comparePassword'>) {
   return UserModel.create(user);
+}
+
+export async function findUserByEmail(email: User['email']) {
+  return UserModel.findOne({ email });
 }
