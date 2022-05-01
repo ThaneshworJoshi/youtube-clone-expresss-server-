f10fcf4ca1c66c04c27e7b580031cde6469c771c
thaneshwor10@gmail.com <thaneshwor10@gmail.com>
2022-05-01 17:26:51 +0545

    add: Cross-Origin-Resource-Policy: cross-origin

--- src/modules/videos/video.controller.ts
+++ src/modules/videos/video.controller.ts
@@ -130,7 +130,7 @@ export async function streamVideoHandler(req: Request, res: Response) {
     'Accept-Ranges': 'bytes',
     'Conent-length': contentLength,
     'Content-Type': `video/${video.extension}`,
-    // 'Cross-Origin_Resource-Policy': 'cross-origin',
+    'Cross-Origin-Resource-Policy': 'cross-origin',
   };
 
   res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
